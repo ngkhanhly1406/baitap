d1a8623d769c28d307627ae5cdcca1514f7c8756
nguyễn khánh ly <khanhly1406@gmail.com>
2022-08-24 16:30:19 +0700

    gg

--- lap4/main1.c
+++ lap4/main1.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include <conio.h>
 void main()
 {
-float p, n, r;
-p = 1000;
-n = 2.5;
-r = 10.5;
-printf("\n Amount is: %f", p*n*r/100);
+	int i = 3, c = 2;
+    printf ("i++ + c is: %d",i++ + c);
+    printf ("++i + c is: %d",++i + c);
 }
